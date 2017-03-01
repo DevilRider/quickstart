6765727370b8865f1a77dbc7339c0f0637ec5efa
L.Yang <lyang@tsingsoft.com.cn>
2017-03-01 11:24:57 +0800

    修正service test注解

--- src/test/java/org/liang/task/CallbackJobTest.java
+++ src/test/java/org/liang/task/CallbackJobTest.java
@@ -6,12 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest()
-@Configuration
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CallbackJobTest {
 
     @Autowired
